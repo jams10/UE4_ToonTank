6809ffb07559b327160723811cf9bd176f793409
jams10 <jamin3710@gmail.com>
2020-09-15 22:25:48 +0900

    00. Downloaded and set project

--- Source/UE4_ToonTank/UE4_ToonTank.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UE4_ToonTank.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4_ToonTank, "UE4_ToonTank" );
