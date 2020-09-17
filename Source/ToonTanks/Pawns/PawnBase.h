b3422db08d7d5308fb0eee3642b76cc020a2dd42
jams10 <jamin3710@gmail.com>
2020-09-17 22:19:14 +0900

    02. Created Tank & Turret Blueprint class derived from C++ class

--- Source/ToonTanks/Pawns/PawnBase.h
+++ Source/ToonTanks/Pawns/PawnBase.h
@@ -6,24 +6,36 @@
 #include "GameFramework/Pawn.h"
 #include "PawnBase.generated.h"
 
+class UCapsuleComponent; // Forward Declaration.
+						 // 클래스 단위로 include 하여 컴파일 타임을 줄임.
+
 UCLASS()
 class TOONTANKS_API APawnBase : public APawn
 {
 	GENERATED_BODY()
 
+private:
+	UPROPERTY()
+	UCapsuleComponent* CapusleComp; // Pawn class에 대한 콜리전 정보 저장.
+	UPROPERTY()
+	UStaticMeshComponent* BaseMesh;
+	UPROPERTY()
+	UStaticMeshComponent* TurretMesh;
+	UPROPERTY()
+	USceneComponent* ProjectileSpawnPoint;
+
 public:
 	// Sets default values for this pawn's properties
 	APawnBase();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 
 };
