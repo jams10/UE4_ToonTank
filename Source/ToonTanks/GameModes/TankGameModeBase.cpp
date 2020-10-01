1bf442b2bdce1a14b4ac65976bec4f43074ca092
jams10 <jamin3710@gmail.com>
2020-10-01 23:36:27 +0900

    14. Created GameMode class

---
+++ Source/ToonTanks/GameModes/TankGameModeBase.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankGameModeBase.h"
+
+void ATankGameModeBase::BeginPlay() 
+{
+    
+}
+
+void ATankGameModeBase::ActorDied(AActor* DeadActor) 
+{
+    
+}
+
+void ATankGameModeBase::HandleGameStart() 
+{
+    
+}
+
+void ATankGameModeBase::HandleGameOver(bool Playerwon) 
+{
+    
+}
+
+
+
+
