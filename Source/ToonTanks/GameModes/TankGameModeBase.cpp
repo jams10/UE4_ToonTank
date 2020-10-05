10095e2043b99254728c11d903169874cd7cdc38
jams10 <jamin3710@gmail.com>
2020-10-05 21:05:33 +0900

    17. GameModeBase added code when actor died

--- Source/ToonTanks/GameModes/TankGameModeBase.cpp
+++ Source/ToonTanks/GameModes/TankGameModeBase.cpp
@@ -2,27 +2,50 @@
 
 
 #include "TankGameModeBase.h"
+#include "ToonTanks/Pawns/PawnTank.h"
+#include "ToonTanks/Pawns/PawnTurret.h"
+#include "Kismet/GameplayStatics.h"
 
 void ATankGameModeBase::BeginPlay() 
 {
-    
+    Super::BeginPlay();
+
+    HandleGameStart();
 }
 
 void ATankGameModeBase::ActorDied(AActor* DeadActor) 
 {
-    UE_LOG(LogTemp, Warning, TEXT("A Pawn Died"));
+    if(DeadActor == PlayerTank)
+    {   
+        PlayerTank->HandleDestruction();
+        HandleGameOver(false);
+    }
+    else if(APawnTurret* DestroyedTurret = Cast<APawnTurret>(DeadActor))
+    {
+        DestroyedTurret->HandleDestruction();
+
+        if(--TargetTurrets == 0)
+        {
+            HandleGameOver(true);
+        }
+    }
 }
 
 void ATankGameModeBase::HandleGameStart() 
 {
-    
+    TargetTurrets = GetTargetTurretCount();
+    PlayerTank = Cast<APawnTank>(UGameplayStatics::GetPlayerPawn(this,0));
+    GameStart();
 }
 
 void ATankGameModeBase::HandleGameOver(bool Playerwon) 
 {
-    
+    GameOver(Playerwon);
 }
 
-
-
-
+int32 ATankGameModeBase::GetTargetTurretCount() 
+{
+    TArray<AActor*> TurretActors;
+    UGameplayStatics::GetAllActorsOfClass(GetWorld(), APawnTurret::StaticClass(), TurretActors);
+    return TurretActors.Num();
+}
--- Source/ToonTanks/GameModes/TankGameModeBase.h
+++ Source/ToonTanks/GameModes/TankGameModeBase.h
@@ -6,9 +6,8 @@
 #include "GameFramework/GameModeBase.h"
 #include "TankGameModeBase.generated.h"
 
-/**
- * 
- */
+class APawnTurret;
+class APawnTank;
 UCLASS()
 class TOONTANKS_API ATankGameModeBase : public AGameModeBase
 {
@@ -16,6 +15,10 @@ class TOONTANKS_API ATankGameModeBase : public AGameModeBase
 
 private:
 
+	APawnTank* PlayerTank;
+	int32 TargetTurrets = 0;
+
+	int32 GetTargetTurretCount();
 	void HandleGameStart();
 	void HandleGameOver(bool Playerwon);
 
